744ecd1a4c319f919d63c865cff5db39451f20b3
Jean Tubay <jean.tubay.92@gmail.com>
2020-03-26 21:10:00 -0400

    set up basic js

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = 'krGnvQlrvdy4ubDduhPYT7141jpgIXLh'
+/*https://api.nytimes.com/svc/search/v2/articlesearch.json?q=obama&begin_date=20140101&end_date=20160101&api-key=krGnvQlrvdy4ubDduhPYT7141jpgIXLh */
+const queryURLbase = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}`;
+
+
